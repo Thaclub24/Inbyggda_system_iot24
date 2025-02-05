c6ca7e9f844133d60fd7420e432f3a18cd74bd15
Thaclub24 <thaclub24_@hotmail.com>
2025-02-05 11:09:23 +0100

    Gjort ovning 4 mapp

---
+++ ovning/ovning4/main/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*Övning:
+Ha en watchdog på 3 sekunder på din mask task
+Testa köra main task utan watchdog
+Sparka watchdog genom att trycka på en knapp*/
+
+void app_main(void)
+{
+
+}
